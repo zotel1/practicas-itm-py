402ac16ec54b1f8c268d4390215b807d37bf87cc
zotel1 <zotelsigel@gmail.com>
2025-09-17 01:52:10 -0300

    arrancamos la parte 1 de la clase 6, seguimos con ciclos

---
+++ clase006/ejercicio050.py
@@ -0,0 +1,19 @@
+"""
+Escribir un programa que permita validar la nota de un examen.
+Se espera que la nota que el usuario ingrese sea un número comprendido entre 0 y 10.
+La misma debe ser ingresada tantas veces como sea necesario 
+hasta que quede comprendida dentro del rango indicado.
+
+Las notas 1 y 3 no usan nunca.
+La nota 0 se reserva para los ausentes.
+Las notas válidas pueden ser un 2 o un valor entre 4 y 10.
+"""
+
+nota = int(input("Nota: "))
+#Expresado por extension
+#while (nota not in (2,4,5,6,7,8,9,10)):
+
+#Expresado por Comprension
+while (nota not in (2,range(4,11))):
+
+    nota = int(input("Nota: "))
