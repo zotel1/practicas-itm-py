d263281810a318e334236d20ebb8943aa85129b8
zotel1 <zotelsigel@gmail.com>
2025-09-17 01:42:03 -0300

    arrancamos con el ejercicio 046

---
+++ clase006/ejercicio046.py
@@ -0,0 +1,16 @@
+"""
+Escribir un programa que permita validar una opcion ingresada.
+Se le preguntara al usuario si desea continuar con alguna informacion de la dorma
+"¿Deseas continuar? [S/N]".
+Se espera que el usuario ingrese una 'S' o una 'N'
+(incluir las minúsculas). 
+La opcion debe ser ingresada tanto como sea necesario hasta que quede comprendida
+dentro de las posibilidades esperadas.
+"""
+
+#opcion = input("Continua")
+
+secuencia = (1,2,3,4,5)
+
+for elemento in secuencia:
+    print(elemento)
