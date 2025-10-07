0aebd272d4e40c951e79bb6567e498e797c51ca7
zotel1 <zotelsigel@gmail.com>
2025-10-07 01:13:26 -0300

    ejemplo001 segunda parte itm trbajando con clases

---
+++ segunda_parte/clase001/ejemplo001.py
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+class Gato:
+    def __init__(self)-> None:
+        self.nombre = ""
+        self.edad = 0
+        self.color = ""
+
+
+
+
+
+
+
+
+def main():
+    a = int(8)
+    print(a)
+
+    g1 = Gato()
+    print(g1)
+    g1.nombre = "Michi"
+    g1.edad = 3
+    g1.color = "Blanco"
+    print(g1.nombre)
+    print(g1.edad)    
+    print(g1.color)
+
+
+
+
+
+
+
+
+main()
